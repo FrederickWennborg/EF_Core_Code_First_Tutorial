8e597a51a3c9aecfea5283c68d92f0f1554a8290
Frederick <frederick.wennborg@gmail.com>
2025-03-12 09:53:46 +0100

    Added Build method to Datainitializer.

--- EF_Core_Code_First_Tutorial/Data/DataInitializer.cs
+++ EF_Core_Code_First_Tutorial/Data/DataInitializer.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EF_Core_Code_First_Tutorial.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace EF_Core_Code_First_Tutorial.Data
 {
-   public class DataInitializer
+    public class DataInitializer
     {
+        public static ApplicationDbContext Build()
+        {
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+             .UseSqlServer(connectionString)
+             .Options;
+
+            var dbContext = new ApplicationDbContext(contextOptions);
+            dbContext.Database.Migrate();
+
+            return dbContext;
+        }
+
+
+
+
+
+
     }
 }
